f83d1d96ff0822ef9ec23be53bd3dd326b5ad298
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2024-08-28 22:06:12 +0530

    database setup and register user functionality

--- app/register/action.ts
+++ app/register/action.ts
@@ -2,6 +2,9 @@
 
 import { passwordMatchSchema } from "@/validation/passwordMatchSchema";
 import { z } from "zod";
+import { hash } from "bcryptjs";
+import db from "@/db/drizzle";
+import { users } from "@/db/usersSchema";
 
 export const registerUser = async ({
   email,
@@ -12,23 +15,41 @@ export const registerUser = async ({
   password: string;
   passwordConfirm: string;
 }) => {
-  const newUserSchema = z
-    .object({
-      email: z.string().email(),
-    })
-    .and(passwordMatchSchema);
+  try {
+    const newUserSchema = z
+      .object({
+        email: z.string().email(),
+      })
+      .and(passwordMatchSchema);
 
-  const newUserValidation = newUserSchema.safeParse({
-    email,
-    password,
-    passwordConfirm,
-  });
+    const newUserValidation = newUserSchema.safeParse({
+      email,
+      password,
+      passwordConfirm,
+    });
 
-  if (!newUserValidation.success) {
-    return {
-      error: true,
-      message:
-        newUserValidation.error.issues[0]?.message ?? "An error occurred",
-    };
+    if (!newUserValidation.success) {
+      return {
+        error: true,
+        message:
+          newUserValidation.error.issues[0]?.message ?? "An error occurred",
+      };
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    await db.insert(users).values({
+      email,
+      password: hashedPassword,
+    });
+  } catch (error: any) {
+    if (error.code === "23505") {
+      return {
+        error: true,
+        message: "An account is already registered with that email address.",
+      };
+    }
+
+    return { error: true, message: "An error occurred" };
   }
 };
